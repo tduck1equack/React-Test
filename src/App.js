317e155eebdae01c97f5116943ee5eff452d626d
DuckToFo <golden4gnvx@gmail.com>
2023-04-04 17:30:20 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Title from './components/Title';
+import React from 'react';
 function App() {
+  const [hobbies, setHobbies] = React.useState([
+    {
+      id: 1,
+      hobby: "📷"
+    }, 
+    {
+      id: 2,
+      hobby: "🎹"
+    }
+  ]);
+
+  const deleteHobby = hobbyId => {
+    const updatedhobbies = hobbies.filter(item => item.id !== hobbyId);
+    setHobbies(updatedhobbies);
+  };
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Rate your hobbies !</h1>
+      {hobbies.map((item, i) => (
+        <li>
+          I
+          <select>
+            <option>likes</option>
+            <option>loves</option>
+          </select>
+          {item.hobby}
+          <button onClick={() => deleteHobby(item.id)}>X</button>
+        </li>
+      ))}
     </div>
   );
 }
---
+++ src/components/Title.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Title.css'
+
+const Title = () => {
+  return <h1 id="title">Title</h1>
+}
+
+export default Title
